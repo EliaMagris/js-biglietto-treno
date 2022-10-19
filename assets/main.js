e1e9a82e98aee09aea7a71c00830c46597732a76
EliaMagris <elia.magris@libero.it>
2022-10-19 14:35:11 +0200

    età confermata

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+function TotalPrice(){
+  
+    const costAtKM = 0.21
+
+    let valueAtKM = document.getElementById ("inputKM"). value;
+
+    document.getElementById ("result").innerHTML = `Total cost is: ${valueAtKM * costAtKM}`
+
+    let valueAge = document.getElementById ("inputAge"). value;
+    
+
+    // let underDiscount = 20%
+
+    // if (underDiscount ) {
+
+    // }
+
+    // let upperDiscount = 40%
+
+}
